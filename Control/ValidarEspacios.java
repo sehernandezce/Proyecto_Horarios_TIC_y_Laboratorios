28c8c0715dac3782ffb1b62d3071e2062e808c61
Sebastian casas <scasasr@unal.edu.co>
2020-10-29 22:05:48 -0500

    llenado tabla solicitar espacio

--- Control/ValidarEspacios.java
+++ Control/ValidarEspacios.java
@@ -2,12 +2,28 @@
 package Control;
 
 import DAO.UsuarioDAO;
+import DAO.EspaciosDAO;
+import Entidad.Usuario;
+import java.sql.ResultSet;
+import java.sql.SQLException;
 
 public class ValidarEspacios {
+    /*
+    EspaciosDAO espacioDao= new EspaciosDAO();
+    public String[][] llenarMatriz(int tipoEspacio,Usuario par) throws SQLException{
+       ResultSet resultSet= espacioDao.leer(par, tipoEspacio);
+       while(resultSet.next()){
+           
+       
+       }
+        
     
+    }
+*/
     public  ValidarEspacios(){
         
     }
+    
     
     
 

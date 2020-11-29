16f9dfa315a0b2edec77ded62c02d083407ef1d8
yeison <ycuervob@unal.edu.co>
2020-11-29 02:57:21 -0500

    correccion

--- Control/Validar_Registro.java
+++ Control/Validar_Registro.java
@@ -33,7 +33,7 @@ public class Validar_Registro {
             return(-4);
         }
          else if(tipoUsuario==2){
-            if(!dao.VerificarCode2(codigo)){
+            if(!dao.VerificarCode(codigo)){
                  return -5;
             }else{
              usuario.setNombreusuarioInstitucional(name);
